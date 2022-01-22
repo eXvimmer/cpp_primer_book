fe00b3c80c1f007d215fd7ff331eba0c5dec038a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-01-22 21:44:53 +0330

    Solve ex_1.4

---
+++ ch01/01_exercises/ex_1.4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main() {
+  cout << "Enter 2 numbers: ";
+  int v1 = 0, v2 = 0;
+  cin >> v1 >> v2;
+  cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << "\n";
+}
