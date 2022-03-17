b139de37e284909be48e6087085a94a320a50b3a
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-17 06:39:04 +0330

    Share a const object among multiple files, by defining it as extern

---
+++ ch02/05_single_instance_of_a_const/file_1.cpp
@@ -0,0 +1,2 @@
+extern const int bufSize = 512;
+// NOTE: we must specify extern in order for bufSize to in other files
---
+++ ch02/05_single_instance_of_a_const/file_1.h
@@ -0,0 +1,6 @@
+extern const int bufSize;
+// NOTE: extern signifies that bufSize is not local to this file and that it's
+// definition will occur elsewhere.
+
+// NOTE: to share a const object among multiple files, you must define the
+// variable as extern
