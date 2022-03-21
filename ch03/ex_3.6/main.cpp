dc0f9bc1f9fd2f3f0957c2eb0ae1772cf71c3c04
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-21 06:09:53 +0330

    Exercise 3.8

--- ch03/ex_3.6/main.cpp
+++ ch03/ex_3.6/main.cpp
@@ -6,12 +6,25 @@ using std::string;
 
 int main() {
   string s{"Mustafa Hayati"};
+  const auto sz{s.size()};
+
   cout << s << '\n';
 
-  for (char &c : s) {
-    c = 'X';
+  decltype(s.size()) i{0};
+
+  while (i < sz) {
+    s[i] = 'X';
+    i++;
   }
 
+  cout << s << '\n';
+
+  s = "Mustafa Hayati";
+  cout << s << '\n';
+
+  for (i = 0; i < sz; i++) {
+    s[i] = 'X';
+  }
   cout << s << '\n';
 
   return 0;
