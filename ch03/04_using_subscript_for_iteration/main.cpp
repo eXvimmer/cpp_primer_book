ee7684c6753e7650b4a6795f39930cf54a540f8f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-21 03:58:26 +0330

    Use subscript to iterate over a string and manipulate it.

---
+++ ch03/04_using_subscript_for_iteration/main.cpp
@@ -0,0 +1,27 @@
+#include <cctype>
+#include <ios>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::isspace;
+using std::string;
+using std::toupper;
+
+int main() {
+  string s{"some string"};
+
+  if (!s.empty()) {
+    s[0] = toupper(s[0]);
+  }
+  cout << s << '\n';
+
+  s = "another string";
+
+  for (decltype(s.size()) i{0}; i != s.size() && !isspace(s[i]); ++i) {
+    s[i] = toupper(s[i]);
+  }
+
+  cout << s << '\n';
+  return 0;
+}
