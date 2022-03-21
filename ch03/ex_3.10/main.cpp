0f1ea5203dc66da457b3b919d3b48ade43f1664d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-21 06:16:15 +0330

    Exercise 3.10

---
+++ ch03/ex_3.10/main.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::string;
+
+int main() {
+  string s{"punc; hello. oh!!! world:\t\nwhat's up?@!#$^&is removed^&*("};
+  cout << s << '\n';
+  string res{};
+
+  cout << "\nAfter cleaning up ==================================\n\n";
+
+  for (const auto c : s) {
+    if (std::ispunct(c))
+      continue;
+
+    res += c;
+  }
+
+  cout << res << '\n';
+
+  return 0;
+}
