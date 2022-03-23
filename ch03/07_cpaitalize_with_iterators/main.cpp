2b0cb4fc6725c1acf118b709afbdd83fb6920773
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-23 04:33:55 +0430

    Capitalize the first word using iterators

--- ch03/07_cpaitalize_with_iterators/main.cpp
+++ ch03/07_cpaitalize_with_iterators/main.cpp
@@ -14,4 +14,14 @@ int main() {
   }
 
   cout << s << '\n';
+
+  s = "another string";
+  cout << s << '\n';
+
+  for (auto it{s.begin()}; it != s.end() && !std::isspace(*it); ++it) {
+    *it = std::toupper(*it);
+  }
+  cout << s << '\n';
+
+  return 0;
 }
